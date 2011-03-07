6ae6e53dd4f4e3a2367c799d65892ad74af8f38f
Joe McCann <joseph.isaac@gmail.com>
2011-03-07 10:00:11 -0600

    Updated CSS to handle new header for mobile web version.

--- clients/web/js/script.js
+++ clients/web/js/script.js
@@ -274,17 +274,6 @@ $(function(){
 			$('#subprint').fadeIn(3000);
 		}, 1000)		
 		
-		var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-3312370-9']);
-		  _gaq.push(['_trackPageview']);
-
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		
-		
 	}
 	
 	// Bindings...
